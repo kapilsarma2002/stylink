8fd734896d20a37ce059ab426c8e0013661a116e
kapilsarma2002 <sarmakapil1818@gmail.com>
2024-05-25 22:53:27 +0530

    schema-update

--- db/schema.ts
+++ db/schema.ts
@@ -1,5 +1,5 @@
 import { randomUUID } from 'crypto'
-import { relations, sql, OneOrMany } from 'drizzle-orm'
+import { sql } from 'drizzle-orm'
 import { pgTable, serial, text, varchar, integer, PgColumn, PgTableWithColumns } from 'drizzle-orm/pg-core'
 
 const id = () =>
@@ -11,8 +11,6 @@ const createdAt = () =>
   text('created_at')
     .default(sql`CURRENT_TIMESTAMP`)
     .notNull()
-
-const date = (name: string) => text(name)
 
 export const user = pgTable('user', {
   id: id(),
@@ -27,4 +25,13 @@ export const product = pgTable('product', {
   description: text('description'),
   price: text('price').notNull(),
   image_url: text('image_url'),
+})
+
+export const order = pgTable('order', {
+  id: id(),
+  createdAt: createdAt(),
+  user_id: id().references(() => user.id).notNull(),
+  product_id: id().references(() => product.id).notNull(),
+  quantity: integer('quantity').notNull(),
+  totalPrice: text('totalPrice').notNull(),
 })
